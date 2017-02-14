8b3f21748708d3630d1a3afb50c6a72361a71278
rubo77 <github@r.z11.de>
2017-02-14 22:28:43 +0100

    gluon-config-mode-ppa: Package to add a checkbox, that you have to agree with the PPA

---
+++ gluon-config-mode-ppa/files/lib/gluon/config-mode/wizard/0510-ppa.lua
@@ -0,0 +1,42 @@
+local cbi = require "luci.cbi"
+local i18n = require "luci.i18n"
+local uci = luci.model.uci.cursor()
+local site = require 'gluon.site_config'
+
+local M = {}
+
+function M.section(form)
+  local s = form:section(cbi.SimpleSection, nil, i18n.translate(
+    "Please accept the PPA."
+    )
+  )
+
+  local o = s:option(cbi.Flag, "_ppa", i18n.translate("I agree with the PPA"))
+  o.default = uci:get_first(gluon-node-info", "owner", "ppa", "")
+  o.rmempty = false
+
+  o.validate = function(self, val)
+    if val == false then
+      self.section.error = {
+        [1] = {
+          i18n.translate(
+            "You didn't accept the picopeering agreement."
+          )
+        }
+      }
+    end
+    return val
+  end
+end
+
+function M.handle(data)
+  if data._ppa ~= nil then
+    uci:set("gluon-node-info", uci:get_first("gluon-node-info", "owner"), "ppa", data._ppa)
+  else
+    uci:delete("gluon-node-info", uci:get_first("gluon-node-info", "owner"), "ppa")
+  end
+  uci:save("gluon-node-info")
+  uci:commit("gluon-node-info")
+end
+
+return M
