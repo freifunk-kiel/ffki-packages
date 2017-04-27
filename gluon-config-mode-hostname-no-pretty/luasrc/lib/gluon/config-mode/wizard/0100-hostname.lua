23fcfea590cd6448de45b8d2ba061e80d853913f
rubo77 <github@r.z11.de>
2017-04-27 21:20:06 +0200

    adapt hostname-no-pretty to gluon-web

--- gluon-config-mode-hostname-no-pretty/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ gluon-config-mode-hostname-no-pretty/luasrc/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -1,21 +1,14 @@
-local cbi = require "luci.cbi"
-local i18n = require "luci.i18n"
-local pretty_hostname = require "pretty_hostname"
-local uci = luci.model.uci.cursor()
+return function(form, uci)
+	local pretty_hostname = require "pretty_hostname"
 
-local M = {}
+	local s = form:section(Section)
+	local o = s:option(Value, "hostname", translate("Node name"))
+	o.default = pretty_hostname.get(uci)
+	o.datatype = "hostname"
 
-function M.section(form)
-  local s = form:section(cbi.SimpleSection, nil, nil)
-  local o = s:option(cbi.Value, "_hostname", i18n.translate("Node name"))
-  o.value = pretty_hostname.get(uci)
-  o.rmempty = false
-  o.datatype = "hostname"
+	function o:write(data)
+		pretty_hostname.set(uci, data)
+	end
+
+	return {'system'}
 end
-
-function M.handle(data)
-  pretty_hostname.set(uci, data._hostname)
-  uci:commit("system")
-end
-
-return M
