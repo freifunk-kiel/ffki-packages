4e756d840d108c5c1129dc126ee2a7774ceec355
rubo77 <github@r.z11.de>
2017-06-10 20:10:25 +0200

    use tabs as indention

--- gluon-config-mode-usb-media/luasrc/lib/gluon/config-mode/wizard/0550-usb-media.lua
+++ gluon-config-mode-usb-media/luasrc/lib/gluon/config-mode/wizard/0550-usb-media.lua
@@ -5,43 +5,40 @@ local uci = require("simple-uci").cursor()
 local M = {}
 
 function M.section(form)
-  local s = form:section(cbi.SimpleSection, nil, translate(
-    'If you want to share your attached USB storage devices, '
-      .. 'here you can enable this and specify the visible path in '
-      .. 'the URL of your Node.'))
+	local s = form:section(cbi.SimpleSection, nil, translate(
+		'If you want to share your attached USB storage devices, '
+			.. 'here you can enable this and specify the visible path in '
+			.. 'the URL of your Node.'))
 
+	local o
 
-  local o
+	o = s:option(Flag, "usbmediasharing", translate("Share your devices"))
+	o.default = uci:get("gluon-usb-media", "settings", "share_device", o.disabled)
+	o.optional = false
 
-  o = s:option(Flag, "usbmediasharing", translate("Share your devices"))
-  o.default = uci:get("gluon-usb-media", "settings", "share_device", o.disabled)
-  o.optional = false
-
-  o = s:option(Value, "usbmediapath", translate("Path"))
-  o.default = uci:get("gluon-usb-media", "settings", "path")
-  o:depends("_usbmediasharing", "1")
-  o.optional = false
-  o.datatype = "hostname"
-  o.description = translatef("e.g. %s", "media")
+	o = s:option(Value, "usbmediapath", translate("Path"))
+	o.default = uci:get("gluon-usb-media", "settings", "path")
+	o:depends("_usbmediasharing", "1")
+	o.optional = false
+	o.datatype = "hostname"
+	o.description = translatef("e.g. %s", "media")
 
 end
 
 function M.handle(data)
 
-  uci:section('gluon-usb-media', 'gluon-usb-media', 'settings',
-            {
-                    share_device = data._usbmediasharing,
-            }
-  )
-  if data._usbmediasharing and data._usbmediapath ~= nil then
-    uci:section('gluon-usb-media', 'gluon-usb-media', 'settings',
-	    {
-		    path = data._usbmediapath,
-	    }
-    )
-  end
-  uci:save("gluon-usb-media")
-  uci:commit("gluon-usb-media")
+	uci:section('gluon-usb-media', 'gluon-usb-media', 'settings', {
+				share_device = data._usbmediasharing,
+			}
+		)
+	if data._usbmediasharing and data._usbmediapath ~= nil then
+		uci:section('gluon-usb-media', 'gluon-usb-media', 'settings', {
+				path = data._usbmediapath,
+			}
+		)
+	end
+	uci:save("gluon-usb-media")
+	uci:commit("gluon-usb-media")
 end
 
 return M
