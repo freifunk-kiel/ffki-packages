80495abe38a623d4241cffa1add24096c1abc6db
rubo77 <github@r.z11.de>
2018-07-03 08:12:11 +0200

    DSGVO konformer Text: verstehen statt zustimmen

--- gluon-config-mode-ppa/files/lib/gluon/config-mode/wizard/0450-ppa.lua
+++ gluon-config-mode-ppa/files/lib/gluon/config-mode/wizard/0450-ppa.lua
@@ -3,12 +3,12 @@ return function(form, uci)
 
 	local s = form:section(
 		Section, nil, pkg_i18n.translate(
-			'Please agree with the <a href="http://www.picopeer.net/" target="_blank">'
-				.. 'Picopeering Agreement (PPA)</a> and be available.'
+			'I understand the <a href="http://www.picopeer.net/" target="_blank">'
+				.. 'Picopeering Agreement (PPA)</a> and will be contactable.'
 			)
 	)	 
 
-	local o = s:option(Flag, "ppa", pkg_i18n.translate("I agree with the PPA"))
+	local o = s:option(Flag, "ppa", pkg_i18n.translate("I understand the PPA"))
 	o.default = uci:get_first("gluon-node-info", "owner", "ppa", "")
 	if o.default ~= nil then
 			o.default = false
